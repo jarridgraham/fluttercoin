342c2a12ca557e39e3cc7497536ec0a900c8004b
jarridgraham <jarrid.graham@gmail.com>
2014-12-10 12:19:27 -0500

    fd94684 commit from Tranz5 HBN

--- src/net.h
+++ src/net.h
@@ -140,6 +140,9 @@ public:
     int64 nReleaseTime;
     int nStartingHeight;
     int nMisbehavior;
+    uint64 nSendBytes;
+    uint64 nRecvBytes;
+    uint64 nBlocksRequested;
 };
 
 class CNetMessage {
@@ -218,6 +221,9 @@ public:
     int64 nLastRecv;
     int64 nLastSendEmpty;
     int64 nTimeConnected;
+    uint64 nBlocksRequested;
+    uint64 nRecvBytes;
+    uint64 nSendBytes;
     int nHeaderStart;
     unsigned int nMessageStart;
     CAddress addr;
@@ -273,6 +279,9 @@ public:
         nLastRecv = 0;
         nLastSendEmpty = GetTime();
         nTimeConnected = GetTime();
+        nSendBytes = 0;
+        nRecvBytes = 0;
+        nBlocksRequested = 0;
         nHeaderStart = -1;
         nMessageStart = -1;
         addr = addrIn;
